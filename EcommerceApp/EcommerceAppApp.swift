80e3ffab2ef2476fef425c1c7c02d46e74f77573
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-17 20:05:21 +0530

    Initial Commit

---
+++ EcommerceApp/EcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppApp.swift
+//  EcommerceApp
+//
+//  Created by sabiq on 17/02/24.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
