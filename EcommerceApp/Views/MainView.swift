0ae1ebc761b36bb24307fbb662ad5443fcb06d1a
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-18 23:16:58 +0530

    edit tab view

--- EcommerceApp/Views/MainView.swift
+++ EcommerceApp/Views/MainView.swift
@@ -8,23 +8,154 @@
 import SwiftUI
 
 struct MainView: View {
+    @State var currentTab:Tab = .Home
+    @Namespace var animation
+    init(){
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
-        @ObservedObject var vm = ProductsViewModel()
+        TabView(selection: $currentTab) {
+            ProductsView()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,maxHeight:.infinity)
+                .background()
+                .tag(Tab.Home)
+            
+         Text("Catagories")
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,maxHeight:.infinity)
+                .background()
+                .tag(Tab.Catagories)
+            
+      
+            
+         Text("Cart")
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,maxHeight:.infinity)
+                .background()
+                .tag(Tab.Cart)
+            
+        }.overlay(
+            HStack(alignment:.bottom,spacing:0){
+                ForEach(Tab.allCases, id: \.rawValue){ tab in
+                    TabButton(tab:tab)
+                    
+                }.padding(.vertical)
+                    .padding(.bottom,getSafeArea().bottom == 0 ? 5 :(getSafeArea().bottom - 15))
+                    .background(Color("SSecondary"))
+                
+            },
+            alignment: .bottom
+            
+        )
+        .ignoresSafeArea(.all, edges: .bottom )
         
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-        .task {
-            await vm.getUsers()
-        }
         
     }
+    
+    func TabButton(tab:Tab) -> some View{
+        GeometryReader{proxy in
+            Button {
+                withAnimation(.spring()){
+                    currentTab = tab
+                }
+            } label: {
+                VStack(spacing:0){
+                    Image(systemName: currentTab == tab ? tab.rawValue + ".fill" : tab.rawValue)
+                        .resizable()
+                        .foregroundStyle(Color("SPrimary"))
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            ZStack{
+                                
+                                if currentTab == tab{
+                                    MaterialEffect(style: .light)
+                                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                        .matchedGeometryEffect(id: "Tab", in: animation)
+                                    Text(tab.TabName)
+                                        .foregroundStyle(.pink)
+                                        .font(.footnote)
+                                        .padding(.top,45)
+                                        
+                                        
+                                    
+                                    
+                                }
+                                
+                                
+                            }
+                        ).contentShape(Rectangle())
+                        .offset(y:currentTab == tab ? -15 : 0)
+                        
+                        
+                    
+                    
+                }
+            }
+
+            
+        }.frame(height:25)
+            
+        
+    }
+    
+    
 }
 
 #Preview {
     MainView()
+}
+
+
+enum Tab : String, CaseIterable{
+    case Home   = "house"
+    case Catagories = "list.bullet.circle"
+   
+    case Cart   = "bag"
+    
+    
+    var TabName : String{
+        switch self{
+        case .Home:
+            return "Home"
+      
+        case .Cart:
+            return "Cart"
+        case .Catagories:
+            return "Catagories"
+        }
+    
+    }
+    
+}
+
+
+extension View{
+    func getSafeArea() -> UIEdgeInsets{
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        else{
+            return .zero
+        }
+        
+        guard let safeArea = screen.windows.first?.safeAreaInsets else {
+            return .zero
+        }
+        
+        return safeArea
+        
+    }
+    
+}
+struct MaterialEffect: UIViewRepresentable{
+    var style : UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: style))
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        
+    }
+    
 }
