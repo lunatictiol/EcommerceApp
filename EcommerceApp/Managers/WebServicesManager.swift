07e2aa4375d76a13257c78dd7da22283fdc65155
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-18 23:21:20 +0530

    function to get images for slider

--- EcommerceApp/Managers/WebServicesManager.swift
+++ EcommerceApp/Managers/WebServicesManager.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 final class WebServicesManager{
-    
+
     static func getProducts() async throws ->[Product]{
         guard let url = URL(string: "\(Constants.baseUrl)products") else{
             throw ErrorCases.invalidUrl
@@ -25,6 +25,23 @@ final class WebServicesManager{
         }
         
     }
+    static func getImagesForSlider() async throws ->[Images]{
+        guard let url = URL(string: "\(Constants.baseUrl)products?limit=5") else{
+            throw ErrorCases.invalidUrl
+        }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let response   = response as? HTTPURLResponse, response.statusCode == 200 else{
+            throw ErrorCases.invalidData
+        }
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode([Images].self, from: data)
+        }catch{
+            throw ErrorCases.invalidData
+        }
+        
+    }
+    
     
     
     
