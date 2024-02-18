e0a9d63e03f2ef4d6c2947e309a6d3cfe31b017c
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-18 23:15:23 +0530

    create product view

--- EcommerceApp/Views/ProductsView/ProductsView.swift
+++ EcommerceApp/Views/ProductsView/ProductsView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct ProductsView: View {
+    @ObservedObject var vm = ProductsViewModel()
+    @State var products :[Product]?
+    @State var images :[Images]?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack{
+                AppBarView()
+                SearchBarView()
+                ImageSliderView(slides:images)
+          
+                HStack{
+                    Text("Top Products")
+                        .bold()
+                    Spacer()
+                    Image(systemName: "circle.grid.2x2.fill")
+                }.padding(.horizontal)
+                ScrollView(.horizontal,showsIndicators: false){
+                    if let products = products{
+                        
+                        HStack(spacing:15){
+                            ForEach(products){ product in
+                             
+                                ProductCardView(product: product )
+                                    .padding(.horizontal,6)
+                            }
+                        }.padding()
+                    }
+                        
+                    
+                        
+                    
+                    
+                    
+                }
+                
+                Spacer()
+                
+            }.task {
+                images = await vm.getImages()
+                products = await vm.getProducts()
+            }
+        }
     }
 }
 
