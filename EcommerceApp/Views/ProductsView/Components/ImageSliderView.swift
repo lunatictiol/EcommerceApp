65ec3472001d6659e7dca3c5ba749d5a4dd00e0d
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-18 23:16:09 +0530

    create image slider

--- EcommerceApp/Views/ProductsView/Components/ImageSliderView.swift
+++ EcommerceApp/Views/ProductsView/Components/ImageSliderView.swift
@@ -8,8 +8,65 @@
 import SwiftUI
 
 struct ImageSliderView: View {
+    @State private var currentIndex = 0
+     var slides : [Images]?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottomLeading ) {
+            if let slides = slides{
+                ZStack(alignment: .trailing){
+                    
+                    AsyncImage(url: URL(string: slides[currentIndex].image), content: { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                        
+                        
+                        
+                    }, placeholder: {
+                        ProgressView()
+                        
+                    })
+                    .frame(width: .infinity, height:180)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    
+                    
+                }
+                
+                HStack{
+                    ForEach(0..<slides.count ,id:\.self){index in
+                        Circle()
+                            .fill(self.currentIndex == index ? Color("SPrimary") : Color("SSecondary") )
+                            .frame(width: 10, height: 10)
+                        
+                    }
+                    
+                }
+                
+            }
+            else{
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .frame(width: .infinity, height:180)
+                    .foregroundStyle(Color("SSecondary"))
+                    .overlay {
+                        ProgressView()
+                    }
+                
+            }
+            
+        }.padding()
+            .onAppear{
+                Timer.scheduledTimer(withTimeInterval:5 , repeats: true, block: { timer in
+                    if self.currentIndex + 1 == 5{
+                        self.currentIndex = 0
+                    }
+                    else{
+                        self.currentIndex += 1
+                    }
+                })
+            }
+        
+        
     }
 }
 
