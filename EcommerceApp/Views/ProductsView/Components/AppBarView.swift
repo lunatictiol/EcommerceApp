957fa50d86d41be04e190e9e3317960163879999
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-18 23:15:00 +0530

    create search bar view

---
+++ EcommerceApp/Views/ProductsView/Components/AppBarView.swift
@@ -0,0 +1,18 @@
+//
+//  AppBarView.swift
+//  EcommerceApp
+//
+//  Created by sabiq on 18/02/24.
+//
+
+import SwiftUI
+
+struct AppBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppBarView()
+}
---
+++ EcommerceApp/Views/ProductsView/Components/SearchBarView.swift
@@ -0,0 +1,62 @@
+//
+//  SearchTextFieldView.swift
+//  EcommerceApp
+//
+//  Created by sabiq on 18/02/24.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    @State private var search:String=""
+    var body: some View {
+        
+        
+        VStack(alignment:.leading,spacing: 5){
+            
+                Text("Find The Most \n innovative")
+                    .font(.largeTitle)
+                    .bold() +
+                Text("Products") .font(.largeTitle)
+                .bold().foregroundStyle(Color("SPrimary"))
+            HStack {
+                HStack{
+                    Image(systemName: "cart.badge.questionmark")
+                        .foregroundStyle(Color("SPrimary"))
+                    TextField("Search Products", text: $search)
+                        .foregroundStyle(Color("SPrimary"))
+                        .padding()
+                    
+                }.padding(.horizontal)
+                    .background(
+                        
+                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                        
+                        
+                            .foregroundStyle(Color("SSecondary")))
+                
+                Image(systemName: "magnifyingglass")
+                    .background(
+                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                            .frame(width:50,height: 50)
+                        
+                            .foregroundStyle(Color("SSecondary")))
+                    .foregroundStyle(Color("SPrimary"))
+                    .padding()
+                
+                
+            }
+        
+            
+            
+            
+        }.padding(.horizontal)
+        
+        
+        
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
