2861bcb4a9b42eb2db556dd4f879371379c8654b
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-18 23:16:20 +0530

    create app bar

--- EcommerceApp/Views/ProductsView/Components/AppBarView.swift
+++ EcommerceApp/Views/ProductsView/Components/AppBarView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct AppBarView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+            HStack{
+                Image(systemName: "location.north.fill")
+                Text("Kashmir,India")
+                    .foregroundStyle(.gray)
+                    .font(.title2)
+                Spacer()
+                
+
+                
+            }.padding(.horizontal)
+        
     }
 }
 
