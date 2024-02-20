e7a4fb2ca53ae69b8b43eeded74de8371f473a4c
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-20 13:41:39 +0530

    search bar logic apply

---
+++ EcommerceApp/Views/ProductsView/Components/SearchResultProductView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultProductView.swift
+//  EcommerceApp
+//
+//  Created by sabiq on 19/02/24.
+//
+
+import SwiftUI
+
+struct SearchResultProductView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchResultProductView()
+}
